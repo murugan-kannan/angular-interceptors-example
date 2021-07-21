592abebb2584f32b2f09a34de7d84d4e0096b238
murugan-kannan <machaendran9221@gmail.com>
2021-07-21 08:59:25 +0530

    added provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CommentsComponent } from './components/comments/comments.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HeaderInterceptors } from './interceptors/header.interceptors';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
   declarations: [AppComponent, CommentsComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HeaderInterceptors, multi: true }
+  ]
 })
 export class AppModule {}
